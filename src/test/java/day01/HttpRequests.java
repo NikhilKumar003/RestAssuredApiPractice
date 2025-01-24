4dec8f27a92ea8d233b895befa08d927ecd70a5c
nikhil <nikhil.kumar@leo1.in>
2025-01-24 16:19:47 +0530

    Created 1st post method and Automated

--- src/test/java/day01/HttpRequests.java
+++ src/test/java/day01/HttpRequests.java
@@ -1,14 +1,15 @@
 package day01;
 
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.HashMap;
 
 import org.testng.annotations.Test;
 
 public class HttpRequests {
 	
-	@Test
+//	@Test
 	void getUsers() {
 		
 		given()
@@ -19,6 +20,26 @@ public class HttpRequests {
 		.then()
 			.statusCode(200)
 			.body("page", equalTo(2))
+			.log().all();
+	}
+	
+	@Test
+	void createUser() {
+		
+		HashMap data = new HashMap();
+		data.put("name", "nikhil");
+		data.put("job", "IT");
+		
+		
+		given()
+			.contentType("application/json")
+			.body(data)
+		
+		.when()
+			.post("https://reqres.in/api/users")
+			
+		.then()
+			.statusCode(201)
 			.log().all();
 	}
 
